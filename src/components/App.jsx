1882dd7a390fd2962f0e513316a369c298eafa26
iliand <iliandaviaud1@gmail.com>
2021-12-26 14:31:31 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,6 @@ import Footer from "./Footer";
 function App() {
   const key = "81cb46c0-65a7-11ec-b453-9f263f29ac19";
   const base = "USD";
-  const url = `https://freecurrencyapi.net/api/v2/latest?apikey=${key}&base_currency=${base}`;
   const [currency, setCurrency] = useState({
     CNY: 6.3,
     EUR: 0.88,
@@ -16,6 +15,7 @@ function App() {
   });
   useEffect(() => {
     setTimeout(() => {
+      const url = `https://freecurrencyapi.net/api/v2/latest?apikey=${key}&base_currency=${base}`;
       fetch(url)
         .then((res) => res.json())
         .then((data) => setCurrency(data.data))
